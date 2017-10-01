faf855901893b60eaba200112eefe22695a891a4
montanoa64 <andmontano89@gmail.com>
2017-09-30 22:54:29 -0500

    setup

---
+++ server.js
@@ -0,0 +1,18 @@
+ Volume in drive C has no label.
+ Volume Serial Number is A6CA-C9E0
+
+ Directory of C:\Users\MyCigga\Documents\github\NodeJSTutorial
+
+09/30/2017  10:31 PM    <DIR>          .
+09/30/2017  10:31 PM    <DIR>          ..
+09/30/2017  10:25 PM    <DIR>          api
+09/30/2017  10:17 PM    <DIR>          node_modules
+09/30/2017  10:17 PM           189,385 package-lock.json
+09/30/2017  10:17 PM               964 package.json
+09/30/2017  10:27 PM    <DIR>          public
+09/30/2017  10:00 PM                43 README.md
+09/30/2017  10:31 PM                 0 server.js
+09/30/2017  10:04 PM                 0 Set-up.txt
+09/30/2017  10:27 PM    <DIR>          src
+               5 File(s)        190,392 bytes
+               6 Dir(s)  223,799,787,520 bytes free
